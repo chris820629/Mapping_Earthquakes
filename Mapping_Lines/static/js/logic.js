c51d2a8a76723199026531cdd789f4a604402bd1
chris820629 <chris820629@gmail.com>
2022-11-15 22:53:50 -0800

    adding draw line feature

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,28 +2,29 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.6214, -122.3790], 5);
 
-// Get data from cities.js
-let cityData = cities;
-  
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location,{
-        radius: city.population/200000,
-        weight: 4,
-        color: 'orange'
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-   });
+// Coordinates for each point to be used in the line.
+let line = [
+    [40.7899, -111.9791],
+    [30.2672, 97.7431],
+    [43.6532,79.3832],
+    [40.7128,74.0060]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    dashArray: '4',
+    color: "blue",
+    weight: 4,
+    opacity: 0.5
+  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/satellite-streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
