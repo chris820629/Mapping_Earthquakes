edbbcd87832cf01d622b45f088072a2bafc0cbc6
chris820629 <chris820629@gmail.com>
2022-11-25 10:52:32 -0800

    add first step logic file

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -52,14 +52,14 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
   Streets: streets,
-  SatelliteStreets: satelliteStreets
+  Satellite: satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -69,26 +69,19 @@ L.control.layers(baseMaps).addTo(map);
 satelliteStreets.addTo(map);
 
 
-let torontoHoods = "https://raw.githubusercontent.com/chris820629/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
+// let torontoHoods = "https://raw.githubusercontent.com/chris820629/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
-let torontoData = "https://raw.githubusercontent.com/chris820629/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json"
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/chris820629/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+// let torontoData = "https://raw.githubusercontent.com/chris820629/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json"
+// // Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/chris820629/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
 // Create a style for the lines.
 let myStyle = {
   color: "#ffffa1",
   weight: 2
 }
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-    weight: 1,
-    color: 'blue',
-    fillColor: 'yellow',
-    onEachFeature: function(feature,layer){
-      layer.bindPopup("<h3>Neighborhood: "+feature.properties.AREA_NAME+"</h3>")
-    }
-  }).addTo(map);
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
